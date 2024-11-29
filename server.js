a4cc47e2428cb2e649a6b8eba8788af8c2e099ef
Serj777rus <133807735+Serj777rus@users.noreply.github.com>
2024-11-29 11:58:14 +0300

    x

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
     credentials: true,
     allowedHeaders: ['Content-Type']
 }))
-app.use(express.static('pdfs'));
+app.use('/pdfs', express.static('pdfs')); // Указывает, что папка доступна по /pdfs
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
